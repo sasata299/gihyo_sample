aa0a014d2131c20dcb19ebc6b739193f0cdf3396
sasata299 <sasata299@livedoor.com>
2010-05-30 19:19:48 +0900

    modify little

--- app/controllers/memcached_controller.rb
+++ app/controllers/memcached_controller.rb
@@ -8,7 +8,9 @@ class MemcachedController < ApplicationController
     @cache['key1'] = 123                      # 数値
     @cache['key2'] = "あいうえお"             # 文字列
     @cache['key3'] = %w(hoge fuga)            # 配列
-    @cache['key4'] = {:foo => 1, :bar => 'a'} # ハッシュ
+    @cache['key4'] = {:foo => 1, :bar => "a"} # ハッシュ
+
+    render :text => "1"
   end
 
   def get
@@ -16,16 +18,20 @@ class MemcachedController < ApplicationController
     p @cache['key2'] # "あいうえお"
     p @cache['key3'] # ["hoge", "fuga"]
     p @cache['key4'] # {:bar=>"a", :foo=>1}
+
+    render :text => "1"
   end
 
   def show
-    if @user = @cache["user#{params[:id]}"]
+    @user = @cache["user#{params[:id]}"]
     unless @user
       @user = User.find(params[:id])
-      @cache.set("user#{params[:id]}", @user, DEFAULT_EXPIRE) # 第3引数にexpiresを指定
+      @cache.set("user#{params[:id]}", @user, DEFAULT_EXPIRE)
     end
 
     # do something
+
+    render :text => "1"
   end
 
   private
